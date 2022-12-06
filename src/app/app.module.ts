8d85e1ff42f43566490d90011cab8738909d4299
Mostafa Wanas <zoho@wanasapps.com>
2022-12-06 21:28:34 +0200

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { MainComponent } from './layout/main/main.component';
+import { LoginComponent } from './login/login.component';
+import { LogoComponent } from './layout/logo/logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    MainComponent,
+    LoginComponent,
+    LogoComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +25,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  globalIsLogin=false;
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { AuthService } from 'src/services/auth/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent {
+  constructor()
+  {
+
+  }
+  onLogin()
+  {
+
+  }
+}
